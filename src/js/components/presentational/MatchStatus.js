dfd7fdd212acc009f47c2a6267bc2048ecf2597f
Adewale Adeoye <whaale_ade@yahoo.com>
2018-04-01 22:21:43 +0100

    Tidy up unused scripts

--- src/js/components/presentational/MatchStatus.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const textStyle = {
-    color: 'red',
-  };
-const MatchStatus = ({match_status}) => (
-    <p className="w3-center w3-padding teamNameHeader" style={textStyle}>FT{match_status}</p>
-);
-export default MatchStatus
--- src/js/components/presentational/ScoreBoard.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const ScoreBoard = ({score}) => (
-    <div className="w3-center teamNameHeader">0{score}</div>
-);
-export default ScoreBoard
